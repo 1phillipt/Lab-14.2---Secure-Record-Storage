15098ac182ee4bc60d9f94e0eee68a22c882fc65
Phillip Thapa <1thapat@gmail.com>
2026-03-31 09:00:44 -0400

    Add user registration and login routes

---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,51 @@
+import { Router, Request, Response } from "express";
+import { User } from "../../models";
+import { signToken } from "../../utils/auth";
+
+const router = Router();
+
+// POST /api/users/register
+router.post("/register", async (req: Request, res: Response) => {
+  try {
+    const user = await User.create(req.body);
+
+    const token = signToken({
+      _id: user._id.toString(),
+      username: user.username,
+      email: user.email,
+    });
+
+    res.status(201).json({ token, user });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// POST /api/users/login
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Can't find this user" });
+    }
+
+    const correctPw = await user.isCorrectPassword(req.body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: "Wrong password!" });
+    }
+
+    const token = signToken({
+      _id: user._id.toString(),
+      username: user.username,
+      email: user.email,
+    });
+
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
